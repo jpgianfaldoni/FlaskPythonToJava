248412755e2677acb96f4d8f06d8aeb3f69a3e1e
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-06-01 12:53:27 -0300

    add firestore db

--- src/app.py
+++ src/app.py
@@ -3,10 +3,18 @@
 # Required Imports
 import os
 from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
+
 
 
 # Initialize Flask App
 app = Flask(__name__)
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
 
 
 @app.route('/add', methods=['POST'])
