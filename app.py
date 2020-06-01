442557aee321dd28b11228ff7bfd7f928225f126
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-06-01 14:02:31 -0300

    add files

---
+++ app.py
@@ -0,0 +1,35 @@
+# app.py
+
+# Required Imports
+import os
+from flask import Flask, request, jsonify
+
+
+# Initialize Flask App
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+    if request.headers.get('Authorization') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "Teste123"})
+
+
+@app.route('/api/post_some_data', methods=['POST'])
+def get_text_prediction():
+    """
+    predicts requested text whether it is ham or spam
+    :return: json
+    """
+    json = request.get_json()
+    print(json)
+    if len(json['text']) == 0:
+        return jsonify({'error': 'invalid input'})
+    numero = int(json['text'])
+    retorno = numero + 1
+    return jsonify({'value': retorno})
+
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
